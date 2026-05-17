062da37cd93e0a6867ed3abfc6645b8583e06a7b
mjddelacruz <michael.d.dela.cruz@gmail.com>
2026-05-17 16:29:17 +1000

    fix: meta robot fix

--- app/og-image.png/route.ts
+++
@@ -1,7 +0,0 @@
-import { createOgImageResponse } from '@/lib/og-image';
-
-export const runtime = 'nodejs';
-
-export async function GET() {
-  return createOgImageResponse();
-}
--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,21 @@
 import type { MetadataRoute } from 'next';
 import { siteConfig } from '@/lib/site';
 
+/** Meta / Facebook link-preview crawlers (Messenger, Facebook, Instagram). */
+const META_CRAWLERS = ['facebookexternalhit', 'Facebot', 'meta-externalagent'];
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: META_CRAWLERS,
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: `${siteConfig.url}/sitemap.xml`,
-    host: siteConfig.url.replace(/^https?:\/\//, ''),
   };
 }
